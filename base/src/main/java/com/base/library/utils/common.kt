46781cb48c35278322511b1ce59e4dacf22baaa4
JaneGavin <1642627230@qq.com>
2025-07-01 11:33:22 +0800

    perf: 添加键盘工具方法

--- base/src/main/java/com/base/library/utils/common.kt
+++ base/src/main/java/com/base/library/utils/common.kt
@@ -1,7 +1,10 @@
 package com.base.library.utils
 
 import android.app.Activity
+import android.content.Context
 import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.base.library.BuildConfig
 
 inline fun logger(tag: String, throwable: Throwable? = null, block: () -> String) {
@@ -20,3 +23,14 @@ val Int.stringRes: String
 
 val Int.colorRes: Int
     get() = Global.cxt.resources.getColor(this)
+
+fun showKeyboard(view: View) {
+    view.requestFocus()
+    val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.showSoftInput(view, InputMethodManager.SHOW_IMPLICIT)
+}
+
+fun hideKeyboard(view: View) {
+    val imm = view.context.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(view.windowToken, 0)
+}
