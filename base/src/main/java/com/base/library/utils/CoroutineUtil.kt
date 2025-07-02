82dcc41fed74e97982252379b2c234d88ede1fdd
JaneGavin <1642627230@qq.com>
2025-07-02 17:21:00 +0800

    perf: 优化全局可处理错误的协程

--- base/src/main/java/com/base/library/utils/CoroutineUtil.kt
+++ base/src/main/java/com/base/library/utils/CoroutineUtil.kt
@@ -5,7 +5,8 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 
 private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-    logger("DatastoreUtilException") { "Caught unhandled exception: $throwable" }
+    logger("CoroutineException") { "Caught unhandled exception: $throwable" }
     // 这里可以添加你的异常处理逻辑，如日志记录、错误上报等
 }
-val safeScope = CoroutineScope(Dispatchers.IO + exceptionHandler)
+val safeIOScope = CoroutineScope(Dispatchers.IO + exceptionHandler)
+val safeMainScope = CoroutineScope(Dispatchers.Main + exceptionHandler)
